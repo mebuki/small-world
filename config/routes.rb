776625858cad188d11d700c01b6fe8de0af2d706
mebuki <mbking0808@gmail.com>
2020-09-23 10:00:51 +0900

    カテゴリー別一覧表

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tops#index"
-  resources :events, only: [:index]
+  resources :events, only: [:index] do
+    collection do
+      get :language, :culture, :hangout, :others
+    end
+  end
 end
