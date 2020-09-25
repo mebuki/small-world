5679d82efffae0c958ed7fa274048c797859996d
mebuki <mbking0808@gmail.com>
2020-09-25 14:34:47 +0900

    削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tops#index"
-  resources :events, only: [:index, :new, :create, :show, :edit, :update] do
+  resources :events do
     collection do
       get :language, :culture, :hangout, :others
     end
