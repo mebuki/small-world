c8cc4be8ec6af490a766b963d28d68b4e0ce75eb
mebuki <mbking0808@gmail.com>
2020-10-03 13:46:30 +0900

    イベント作成・編集画面の作成

--- app/models/purpose.rb
+++ app/models/purpose.rb
@@ -1,9 +1,9 @@
 class Purpose < ActiveHash::Base
   self.data = [
     {id: 0, name: "what’s your purpose?" },
-    {id: 1, name: "言語交換 / Language Exchange"},
-    {id: 2, name: "異文化交流 / Experience a different culture"},
-    {id: 3, name: "おでかけ / Hang out"},
-    {id: 4, name: "その他 / Others"}
+    {id: 1, name: "Language Exchange / 言語交換"},
+    {id: 2, name: "Experience a different culture / 異文化交流"},
+    {id: 3, name: "Hang out / おでかけ"},
+    {id: 4, name: "Others / その他"}
   ]
 end
