9131d7a031bbeef27bc20de04988b476023db125
mebuki <mbking0808@gmail.com>
2020-10-04 15:50:32 +0900

    ツイート作成・編集画面の作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -46,7 +46,7 @@ class TweetsController < ApplicationController
   private
 
   def tweet_params
-    params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:image, :title, :text).merge(user_id: current_user.id)
   end
 
   def set_tweet
